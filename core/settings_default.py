653805ece8c5611aa18a6edd4cff56e448b67e3a
Huelder <huelderantuness@gmail.com>
2020-10-28 18:20:42 -0300

    add: production config

---
+++ core/settings_default.py
@@ -0,0 +1,155 @@
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+# Application definition
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+INSTALLED_APPS = [
+
+    # 3rd party
+    'django_bleach',
+    'modeltranslation',
+    'ckeditor',
+    'crispy_forms',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'whitenoise.runserver_nostatic',
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'orders',
+]
+
+SITE_ID = 1
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
+LOGIN_REDIRECT_URL = '/orders/'
+ACCOUNT_ADAPTER = 'accounts.adapters.DefaultAccountAdapter'
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'en'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('pt', _('Portuguese')),
+)
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+
+STATICFILES_FINDER = ['django.contrib.staticfiles.finders.FileSystemFinder',
+                      ' django.contrib.staticfiles.finders.AppDirectoriesFinder']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# E-mail settings
+
+EMAIL_HOST = env('EMAIL_HOST')
+MAIL_PORT = env('MAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+# EMAIL_USE_SSL = env('EMAIL_USE_SSL')
+EMAIL_TIMEOUT = 50
+
+
+# Sanitize
+
+BLEACH_DEFAULT_WIDGET = 'ckeditor.widgets.CKEditorWidget'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'skin': 'moono-lisa',
+        # 'skin': 'office2013',
+        'toolbar_Basic': [{'name': 'basicstyles',
+                           'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+                          {'name': 'styles', 'items': [
+                              'Styles', 'Format', 'Font', 'FontSize']},
+                          {'name': 'colors', 'items': [
+                              'TextColor', 'BGColor']},
+                          {'name': 'paragraph',
+                           'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
+                                     'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
+                                     'Language']}
+                          ],
+        'toolbar': 'Basic',
+    }
+}
--- core/settings_production.py
+++ core/settings_production.py
@@ -13,8 +13,9 @@ from django.utils.translation import ugettext_lazy as _
 import os
 import environ
 from pathlib import Path
-
-
+from core.settings_default import *
+import django_on_heroku
+from django.core.management.utils import get_random_secret_key
 env = environ.Env()
 environ.Env.read_env()
 
@@ -28,76 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': 'full',
-        'height': 300,
-        'width': 'full',
-    },
-}
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-INSTALLED_APPS = [
-
-    # 3rd party
-    'django_bleach',
-    'modeltranslation',
-    'ckeditor',
-    'crispy_forms',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'whitenoise.runserver_nostatic',
-    # django
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'orders',
-]
-
-SITE_ID = 1
-
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
-]
-
-ROOT_URLCONF = 'core.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
@@ -113,102 +46,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-
-
-LOGIN_REDIRECT_URL = '/orders/'
-ACCOUNT_ADAPTER = 'accounts.adapters.DefaultAccountAdapter'
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-LANGUAGE_CODE = 'en'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
-
-
-LANGUAGES = (
-    ('en', _('English')),
-    ('pt', _('Portuguese')),
-)
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-
-STATICFILES_FINDER = ['django.contrib.staticfiles.finders.FileSystemFinder',
-                      ' django.contrib.staticfiles.finders.AppDirectoriesFinder']
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# E-mail settings
-
-EMAIL_HOST = env('EMAIL_HOST')
-MAIL_PORT = env('MAIL_PORT')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-# EMAIL_USE_TLS = env('EMAIL_USE_TLS')
-# EMAIL_USE_SSL = env('EMAIL_USE_SSL')
-EMAIL_TIMEOUT = 50
-
-
-# Sanitize
-BLEACH_DEFAULT_WIDGET = 'ckeditor.widgets.CKEditorWidget'
-
-CKEDITOR_CONFIGS = {
-    'default': {
-        'skin': 'moono-lisa',
-        # 'skin': 'office2013',
-        'toolbar_Basic': [{'name': 'basicstyles',
-                           'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
-                          {'name': 'styles', 'items': [
-                              'Styles', 'Format', 'Font', 'FontSize']},
-                          {'name': 'colors', 'items': [
-                              'TextColor', 'BGColor']},
-                          {'name': 'paragraph',
-                           'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
-                                     'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
-                                     'Language']}
-                          ],
-        'toolbar': 'Basic',
-    }
-}
-
-
 # SECURITY
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_REFERRER_POLICY = 'same-origin'
@@ -218,3 +55,5 @@ SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+django_on_heroku.settings(locals())
